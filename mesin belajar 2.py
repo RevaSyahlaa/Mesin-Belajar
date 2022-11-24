d965d18e744beadff08d9d2b7f11498bd13f66b8
RevaSyahlaa <114666789+RevaSyahlaa@users.noreply.github.com>
2022-11-24 17:42:39 +0700

    Upload File
    menambhakan 2 file menggunakan 2 buah metodre dengan total 10 data

---
+++ mesin belajar 2.py
@@ -0,0 +1,23 @@
+from sklearn.neighbors import NearestCentroid
+#database : gerbang logika AND
+# x = data, Y = Target
+
+x = [[0,0],[0,1],[1,0],[1,1],[0,2],[2,1],[1,2],[2,2],[0,3]]
+y = [0,0,0,1,2,1,2,2,3]
+
+# Training and Classify
+clf = NearestCentroid()
+clf = clf.fit(x,y)
+
+#prediksi
+print ("logika AND Metode Decision Tree")
+print ("Logika = Prediksi")
+print ("0 0 = ", clf.predict([[0,0]]))
+print ("0 1 = ", clf.predict([[0,1]]))
+print ("1 0 = ", clf.predict([[1,0]]))
+print ("1 1 = ", clf.predict([[1,1]]))
+print ("0 2 = ", clf.predict([[0,2]]))
+print ("2 1 = ", clf.predict([[2,1]]))
+print ("1 2 = ", clf.predict([[1,2]]))
+print ("2 2 = ", clf.predict([[2,2]]))
+print ("0 3 = ", clf.predict([[0,3]]))
